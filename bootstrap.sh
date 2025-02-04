cafcbfb77dff3bbccbef22ed567a47fa71334f87
SebastianKleinhans <120041880+SebastianKleinhans@users.noreply.github.com>
2025-02-04 11:13:56 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt update && sudo apt install -y git ansible
-git clone git@github.com:SebastianKleinhans/ansible-wsl-setup.git ~/ansible-wsl-setup
+git clone git@github.com:SebastianKleinhans/dotfiles.git ~/ansible-wsl-setup
 cd ~/ansible-wsl-setup
 ansible-playbook setup.yml
